4415d24be170aca8b4dde5a84abceb320c27f163
James Slagle <jslagle@redhat.com>
2021-10-13 16:14:33 -0400

    Use

--- scripts/d-bootstrap.sh
+++ scripts/d-bootstrap.sh
@@ -4,5 +4,5 @@ set -eux
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-$(SCRIPT_DIR)/d-pip-install.sh
+${SCRIPT_DIR}/d-pip-install.sh
 /opt/directord/bin/directord bootstrap --catalog directord/tools/directord-inventory-catalog.yaml --catalog directord/tools/directord-dev-bootstrap-messaging-catalog.yaml
