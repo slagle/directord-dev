fb2335c445e91479c504433fd516db964e2f40ff
James Slagle <jslagle@redhat.com>
2021-10-13 16:22:10 -0400

    Use

--- scripts/d-bootstrap.sh
+++ scripts/d-bootstrap.sh
@@ -6,4 +6,4 @@ INVENTORY_CATALOG=${1-"directord/tools/directord-inventory-catalog.yaml"}
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 ${SCRIPT_DIR}/d-pip-install.sh
-/opt/directord/bin/directord bootstrap --catalog ${INVENTORY_CATALOG} --catalog directord/tools/directord-dev-bootstrap-messaging-catalog.yaml $@
+/opt/directord/bin/directord bootstrap --catalog ${INVENTORY_CATALOG} --catalog directord/tools/directord-dev-bootstrap-messaging-catalog.yaml
