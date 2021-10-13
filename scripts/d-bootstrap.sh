89e2aedb8ed22990a01a6d398ab1ca210a678cf1
James Slagle <jslagle@redhat.com>
2021-10-13 16:23:22 -0400

    Update

--- scripts/d-bootstrap.sh
+++ scripts/d-bootstrap.sh
@@ -3,7 +3,8 @@
 set -eux
 
 INVENTORY_CATALOG=${1-"directord/tools/directord-inventory-catalog.yaml"}
+KEY_FILE=${2-"~/.ssh/id_rsa"}
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 ${SCRIPT_DIR}/d-pip-install.sh
-/opt/directord/bin/directord bootstrap --catalog ${INVENTORY_CATALOG} --catalog directord/tools/directord-dev-bootstrap-messaging-catalog.yaml
+/opt/directord/bin/directord bootstrap --catalog ${INVENTORY_CATALOG} --catalog directord/tools/directord-dev-bootstrap-messaging-catalog.yaml --key-file $KEY_FILE
